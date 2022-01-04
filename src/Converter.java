93350d897e788b877745e24dc724acf60e7a65f7
Marina_Kuznetsova <marussia.usa@yandex.ru>
2022-01-04 15:22:22 +0300

    внесены исправления

--- src/Converter.java
+++ src/Converter.java
@@ -1,18 +1,17 @@
 public class Converter {
+    double toDistance;
+    double toCal;
 
-        double toDistance;
-        double toCal;
-
-        Converter(double distance, double cal) {
-            toDistance = distance;
-            toCal = cal;
-        }
-
-        double convertToDistanceKm (int completedSteps) {
-            return (double) completedSteps * toDistance/1000;
-        }
-
-        double convertToKkal (int completedSteps) {
-            return (double) completedSteps * toCal/1000;
-        }
+    Converter(double distance, double cal) {
+        toDistance = distance;
+        toCal = cal;
     }
+
+    double convertToDistanceKm (int completedSteps) {  // возвращает пройденную дистанцию (в км);
+        return (double) completedSteps * toDistance/1000;
+    }
+
+    double convertToKkal (int completedSteps) {   // возвращает количество сожжённых килокалорий
+        return (double) completedSteps * toCal/1000;
+    }
+}
--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class StepTracker {
     HashMap<String, int[]> stepInfo = new HashMap<>();
     int stepsGoal = 10000;
+    Scanner scanner = new Scanner(System.in);
 
     StepTracker() {
         int[] days = new int[30];
@@ -22,23 +24,38 @@ public class StepTracker {
     }
 
 
-    void saveStepsPerDay(String month, int day, int stepsCompleted) {
-        int[] days = stepInfo.get(month.toLowerCase());
+    void saveStepsPerDay() {  // Ввести и сохранить количествo шагов за определённый день
+        System.out.println("Укажите название месяца");
+        String month = checkedMonth();
+        System.out.println("Укажите день от 1 до 30");
+        int day = checkedDay();
+        System.out.println("Сколько Вы прошли в указанный день?");
+        int stepsCompleted = checkedCompletedSteps();
+        int[] days = stepInfo.get(month);
         days[day - 1] += stepsCompleted;
-        stepInfo.put(month.toLowerCase(), days);
-        if (month.equalsIgnoreCase("март") || month.equalsIgnoreCase("август")) {
-            System.out.println("Сохранено. " + day + " " + month.toLowerCase() + "а Вы прошли " + days[day - 1] + " шагов");
-        } else if (month.equalsIgnoreCase("май")) {
-            System.out.println("Сохранено. " + day + " " + "мая" +
-                    " Вы прошли " + days[day - 1] + " шагов");
-        } else {
-            month = month.toLowerCase();
-            System.out.println("Сохранено. " + day + " " + month.replace('ь', 'я') +
-                    " Вы прошли " + days[day - 1] + " шагов");
-        }
+        stepInfo.put(month, days);
+        if (month.equals("март") || month.equals("август")) {
+            System.out.println("Сохранено. " + day + " " + month + "а Вы прошли " + days[day - 1] + " шагов");
+        } else if (month.equals("май")) {
+            System.out.println("Сохранено. " + day + " " + "мая" + " Вы прошли " + days[day - 1] + " шагов");
+        } else System.out.println("Сохранено. " + day + " " + month.replace('ь', 'я') +
+                " Вы прошли " + days[day - 1] + " шагов");
     }
 
-    void getStatistic(String month) {
+    void getStatistics(Converter converter) {    // Подсчёт и вывод статистики за указанный пользователем месяц
+        System.out.println("Укажите месяц, за который Вы хотите получить статистику");
+        String month = checkedMonth();
+        getStatisticsByDay(month);
+        int completedSteps = completedStepsPerMonth(month);
+        System.out.println("За месяц Вы прошли " + completedSteps + " шагов");
+        System.out.println("Максимальное количество шагов за день составило: " + getMaxSteps(month));
+        System.out.println("Среднее количество шагов за день составило: " + getAveragePerMonth(month));
+        System.out.println("Пройденная дистанция: " + converter.convertToDistanceKm(completedSteps) + " км");
+        System.out.println("Количество сожжённых килокалорий: " + converter.convertToKkal(completedSteps));
+        System.out.println("Лучшая серия: " + getTheBestSeries(month) + " дней подряд");
+    }
+
+    void getStatisticsByDay (String month) {  // печатает количество пройденных шагов по дням
         System.out.println("Количество пройденных шагов по дням: ");
         int[] days = stepInfo.get(month.toLowerCase());
         for (int i = 0; i < days.length; i++) {
@@ -49,8 +66,8 @@ public class StepTracker {
         System.out.println();
     }
 
-    int completedStepsPerMonth(String month) {
-        int[] days = stepInfo.get(month.toLowerCase());
+    int completedStepsPerMonth(String month) {  // возвращает сумму пройденных шагов за месяц
+        int[] days = stepInfo.get(month);
         int sum = 0;
         for (int i = 0; i < days.length; i++) {
             sum += days[i];
@@ -58,8 +75,8 @@ public class StepTracker {
         return sum;
     }
 
-    int getMaxSteps(String month) {
-        int[] days = stepInfo.get(month.toLowerCase());
+    int getMaxSteps(String month) {   // возвращает максимальное пройденное количество шагов в месяце
+        int[] days = stepInfo.get(month);
         int maxStepsPerDays = 0;
         for (int i = 0; i < days.length; i++) {
             if (days[i] > maxStepsPerDays) {
@@ -69,8 +86,8 @@ public class StepTracker {
         return maxStepsPerDays;
     }
 
-    double getAveragePerMonth(String month) {
-        int[] days = stepInfo.get(month.toLowerCase());
+    double getAveragePerMonth(String month) {   // возвращает среднее количество шагов за месяц
+        int[] days = stepInfo.get(month);
         int sum = 0;
         for (int i = 0; i < days.length; i++) {
             sum += days[i];
@@ -78,8 +95,8 @@ public class StepTracker {
         return (double) sum / days.length;
     }
 
-    int getTheBestSeries(String month) {
-        int[] days = stepInfo.get(month.toLowerCase());
+    int getTheBestSeries(String month) {   // возвращает лучшую серию
+        int[] days = stepInfo.get(month);
         ArrayList<Integer> series = new ArrayList<Integer>();
         for (int i = 0; i < days.length; i++) {
             if (days[i] > stepsGoal) {
@@ -102,15 +119,62 @@ public class StepTracker {
         else return 0;
     }
 
-    void changeStepsGoal(int newStepsGoal) {
-        stepsGoal = newStepsGoal;
-        System.out.println("Теперь Ваша цель " + stepsGoal + " шагов в день");
+    void changeStepsGoal() {   // меняет целевое количество шагов
+        System.out.println("Введите количество шагов:");
+        int newStepsGoal = scanner.nextInt();
+        if (newStepsGoal < 0) {
+            System.out.println("Значение не может быть отрицательным, попробуйте снова");
+            return;
+        } else if (newStepsGoal == 0) {
+            System.out.println("Цель не может быть 0 шагов в день, попробуйте снова");
+            return;
+        } else {
+            stepsGoal = newStepsGoal;
+            System.out.println("Теперь Ваша цель " + stepsGoal + " шагов в день");
+        }
     }
 
-    boolean checkedMonth (String month) {
+    String checkedMonth () {   // проверить правильность ввода месяца и вернуть значение
+        String month = scanner.next();
         if (stepInfo.containsKey(month.toLowerCase())) {
-            return true;
+            return month.toLowerCase();
         }
-        else return false;
+        else {
+            do {
+                System.out.println("Неправильно указан месяц, попробуйте снова");
+                month = scanner.next();
+            }
+            while (!(stepInfo.containsKey(month.toLowerCase())));
+        }
+        return month.toLowerCase();
+    }
+
+    int checkedDay () {   // проверить правильность ввода дня и вернуть значение
+        int day = scanner.nextInt();
+        if (day >= 1 && day <= 30) {
+            return day;
+        } else {
+            do {
+                System.out.println("Можно указать только от 1 до 30, попробуйте снова");
+                day = scanner.nextInt();
+            }
+            while (day < 1 || day > 30);
+            return day;
+        }
+    }
+
+    int checkedCompletedSteps () {   // проверить правильность ввода количества шагов и вернуть значение
+        int completedSteps = scanner.nextInt();
+        if (completedSteps >= 1) {
+            return completedSteps;
+        } else {
+            do {
+                System.out.println("Значение не может быть отрицательным или равным 0.");
+                System.out.println("Попробуйте снова.");
+                completedSteps = scanner.nextInt();
+            }
+            while (completedSteps <= 0);
+            return completedSteps;
+        }
     }
 }
